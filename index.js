da2acc198808d61c605edb4915e53d50a8a4129b
Žiga Vukčevič <ziga@be-codified.com>
2017-02-11 13:02:44 +0100

    First cats scrape

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
 const request = require("tinyreq");
+const cheerio = require('cheerio')
 
-request("http://ionicabizau.net/", function (err, body) {
-    console.log(err || body); // Print out the HTML
+var url = 'http://zavetisce-ljubljana.si/sl-SI/1433/iscemo-nov-dom-posvoji-macko';
+
+request(url, function (err, html) {
+    // console.log(err || html);
+
+    const $ = cheerio.load(html);
+    const noOfCats = $('.animal_renderW .AnLi_item .AnLi_itemM');
+
+     var cats = [];
+
+    noOfCats.each(function(i, element) {
+        // console.log(i, '===================================');
+
+        // title
+        const name = $(this)
+            .children()[0]      // .AnLi_Title
+            .children[1]        // .AnLi_TitleL
+            .children[1]        // a
+            .children[0].data;  // name
+
+
+        // gender
+        const gender = $(this)
+            .children()[0]      // .AnLi_Title
+            .children[3]        // .AnLi_TitleR
+            .children[1]        // img
+            .attribs['alt'];    // gender
+
+
+        // image
+        const image = $(this)
+            .children()[1]      // .AnLi_Img
+            .children[1]        // a
+            .children[1]        // img
+            .attribs['src'];    // src
+
+        isFemaleIgnored = true;
+
+        if (gender === 'Samec') {
+
+            console.log('-----------------------------------------------', i);
+
+            cats[i] = {
+                name: name,
+                gender: gender,
+                image: image };
+        }
+    });
+
+    console.log(cats);
+    console.log('=================================');
+    console.log('Scraped info:');
+    console.log('---------------------------------');
+    console.log('Cat records: ', cats.length)
 });
